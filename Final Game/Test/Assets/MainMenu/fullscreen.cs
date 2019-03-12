c584403735b7120a57951fc99e628ceffddfeb28
matthewmungall96 <matthewmungall96@gmail.com>
2019-03-12 16:17:31 +0000

    Added All

---
+++ Final Game/Test/Assets/MainMenu/fullscreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fullscreen : MonoBehaviour {
+
+    public GameObject toggle;
+
+    private void Start()
+    {
+       toggle.GetComponent<Toggle>().isOn = Screen.fullScreen;
+    }
+
+    // Update is called once per frame
+    public void Toggle () {
+        Screen.fullScreen = !Screen.fullScreen;
+        if (Screen.fullScreen)
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+        }
+        else
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+        }
+    }
+}
