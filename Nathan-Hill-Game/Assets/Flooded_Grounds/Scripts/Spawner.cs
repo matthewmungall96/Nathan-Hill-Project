69ed980d745e0d6b01924fb83b12b6c778f46d9f
NikolaV96 <32327177+NikolaV96@users.noreply.github.com>
2019-04-15 00:12:56 +0100

    added the spawn point

---
+++ Nathan-Hill-Game/Assets/Flooded_Grounds/Scripts/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countDown = 2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+        countDown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        SpawnEnemy();
+        yield return null;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
