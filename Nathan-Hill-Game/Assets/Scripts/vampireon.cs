c78dbad1e557235c39717fa79dda698812f05280
Leo <leo-mas-@hotmail.com>
2019-04-18 13:50:14 +0100

    doors and animations

---
+++ Nathan-Hill-Game/Assets/Scripts/vampireon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vampireon : MonoBehaviour
+{
+
+    public GameObject vampire;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.ToLower() == "player")
+        {
+            vampire.SetActive(true);
+        }
+    }
+}
